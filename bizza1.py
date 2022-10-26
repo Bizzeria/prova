4486233070c59151d1f9c20c1ce7b436e1ee9962
Bizzeria <zungrone.lorenzo@iisgalvanimi.edu.it>
2022-10-26 10:59:54 +0000

    versione 4

--- bizza1.py
+++ bizza1.py
@@ -1,5 +1,27 @@
 x = 5
 y = "Bizza"
 print(x)
-print(y)
+print(y)                                 #1.1 come stampare una variabile
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)                                #1.2 come cambiare contenuto di variabile
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0    
+                                        #1.3 tipi di variabile (testo, numerico,decimale)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))                         #1.4 stampa il tipo della variabile selezionata 
+
+x = "John"
+# is the same as
+x = 'John'                             #1.5 tipi di virgolette ammesse 
+
+a = 4
+A = "Sally"
+#A will not overwrite a                #1.6 a ed A sono considerate variabili diverse    
 
---
+++ bizza2.py
@@ -0,0 +1,23 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)                                 #2.1 creare e stampare una lista
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)                                 #2.2 sono ammesse variabili doppie
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))                            #2.3 lista numerata 
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]                    #2.4 tipi di variabili ammesse
+
+list1 = ["abc", 34, True, 40, "male"]           #2.5 variabili diverse sono ammesse anche nella stessa lista
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))                             #2.6 stampare tipo di lista (se variabili tutte uguali)
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)                                 #2.7 boh 
+
+
+            
