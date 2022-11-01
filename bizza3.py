80dc31dcdc8f092b048d7239ad6b676942b2018d
Bizzeria <zungrone.lorenzo@iisgalvanimi.edu.it>
2022-11-01 19:32:04 +0000

    versione ruscibizza3

--- bizza3.py
+++ bizza3.py
@@ -7,7 +7,7 @@ if b > a:
 a = 33
 b = 200
 if b > a:
-print("b is greater than a") # you will get an error #3.2 scrivere print staccato 
+print("b is greater than a") # you will get an error 3.2 scrivere print staccato 
 
 a = 33
 b = 33
@@ -33,3 +33,40 @@ else:
   print("b is not greater than a")                   #3.5 anche senza elif
 
   if a > b: print("a is greater than b")             #3.6 anche senza dichiarare le variabili 
+
+  a = 2
+b = 330
+print("A") if a > b else print("B")                  #3.7 con una sola condizione da verificare
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+                                                     #3.8 tre condizioni in una riga sola
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")                  #3.9 operatore "and"
+
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")   #3.10 operatore "or"
+
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")                     #3.11 operatore if annidato 
+
+a = 33
+b = 200
+
+if b > a:
+  pass                                             #3.12 operatore "pass" se if vuoto
+
