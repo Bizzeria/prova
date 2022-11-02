2d21ab3c510778ff87ac80e28603004ba1fbab83
Bizzeria <zungrone.lorenzo@iisgalvanimi.edu.it>
2022-11-02 15:50:18 +0000

    esercizio 2

--- esercizi.py
+++ esercizi.py
@@ -1,3 +1,4 @@
+#es 1
 x = "cane"
 y = "mucca"
 z = "cavallo"
@@ -5,3 +6,12 @@ s = "coccodrillo"
 t = "falena"
 
 print (x , y , z , t , s)
+
+#es 2 
+
+x = 5
+y = 34
+if x > y: 
+    print ("ciao")
+else: 
+    print ("arrivederci")
