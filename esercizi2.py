0dcab38b6cd2e845cd7723ec2bff50a7b1de6f3b
Bizzeria <zungrone.lorenzo@iisgalvanimi.edu.it>
2022-11-22 21:00:50 +0000

    versione rusci nabbo

--- esercizi2.py
+++ esercizi2.py
@@ -45,18 +45,52 @@ print ("°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø")
 #es E
 
 def primo(n):
-    if n == 0:
-        print (n , "non è un numero primo")
-    
-    if n == 1:
+    if n < 2:
         print (n , "non è un numero primo")   
-        
+    if n == 2:
+        print ("true")    
     for y in range(2,n):
         if n%y == 0:
             print (n , "non è un numero primo")
             break
         else:
-            print("True")
+            print("true")
             break
 
 primo(17)
+
+prm = int(input("Scrivere un numero: "))
+def primi(prm):      
+    if prm < 2:
+        print (prm, "non è un numero primo")   
+    if prm == 2:
+        print("primo")
+    for s in range(2,prm):
+        if prm%s == 0:
+            print (prm , "non è un numero primo")
+            break
+        else:
+            print ("primo")
+        break
+
+primi (prm)
+
+print ("°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø")
+
+#es F
+
+Sbommi = []
+
+def lista(Sungro):
+    rn= int(input("Quanti numeri si vuole inserire nella lista? "))
+    for c in range(rn):
+            num = int(input("Scrivere un numero: "))
+            Sbommi.append(num)
+            for f in range (num):
+                print ("*")
+
+lista (Sbommi)
+
+print ("°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø")
+
+#es G
